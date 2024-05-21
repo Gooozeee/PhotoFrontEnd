16e3eeee228bdfb1c6e6d37a3dcaea3f74426fb8
Gooozeee <Michalguzym@gmail.com>
2024-05-21 15:28:17 +0100

    Feat: Loading the images dynamically onto the album page

--- src/Pages/SingleAlbumPage.tsx
+++ src/Pages/SingleAlbumPage.tsx
@@ -1,16 +1,78 @@
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import "../styles/SingleAlbumPageStyles.css";
+import SingleAlbumImage from "../components/SingleAlbumImage";
 
 const SingleAlbumPage = () => {
   const queryParameters = new URLSearchParams(window.location.search);
+  const albumName = queryParameters.get("album");
 
-  const albumName = queryParameters.get("album")
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      let imagesGlob: Record<string, () => Promise<{ default: string }>>;
+
+      switch (albumName) {
+        case "Birds":
+          imagesGlob = import.meta.glob("../assets/Birds/*") as Record<
+            string,
+            () => Promise<{ default: string }>
+          >;
+          break;
+        case "Rally":
+          imagesGlob = import.meta.glob("../assets/Rally/*") as Record<
+            string,
+            () => Promise<{ default: string }>
+          >;
+          break;
+        default:
+          console.log("Not found folder");
+          return;
+      }
+
+      const imageUrls = await Promise.all(
+        Object.keys(imagesGlob).map(async (key) => {
+          const module = await imagesGlob[key]();
+          return module.default;
+        })
+      );
+
+      setImages(imageUrls);
+    };
+
+    loadImages();
+  }, [albumName]);
 
   return (
-    <body className="album-page-container">
+    <div className="album-page-container">
       <NavBar />
-      <h1>Album: {albumName}</h1>
-    </body>
+
+      <div className="image-gallery">
+        <div className="column">
+          {images.map((src, index) =>
+            index % 2 === 1 ? (
+              <SingleAlbumImage
+                key={index}
+                imageSource={src}
+                imageDescription={`Image ${index}`}
+              />
+            ) : null
+          )}
+        </div>
+        <div className="column">
+          {images.map((src, index) =>
+            index % 2 === 0 ? (
+              <SingleAlbumImage
+                key={index}
+                imageSource={src}
+                imageDescription={`Image ${index}`}
+              />
+            ) : null
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/SingleAlbumImage.tsx
+++ src/components/SingleAlbumImage.tsx
@@ -1,23 +1,19 @@
-import { Link } from "react-router-dom";
 import "../styles/GalleryImageStyles.css";
 
 interface Props {
   imageSource: string;
   imageDescription: string;
-  albumName: string;
 }
 
-const GalleryImage = ({ imageSource, imageDescription, albumName }: Props) => {
+const SingleAlbumImage = ({ imageSource, imageDescription }: Props) => {
   return (
     <div className="image-item">
-      <Link to={`/singleAlbum?album=${albumName}`} className="link">
         <img src={imageSource} alt={imageDescription} />
         <div className="overlay">
           <span>{imageDescription}</span>
         </div>
-      </Link>
     </div>
   );
 };
 
-export default GalleryImage;
+export default SingleAlbumImage;
