49ed4a473ae6345ae77bb82af893c516f5a8b1be
Gooozeee <Michalguzym@gmail.com>
2024-05-22 12:49:36 +0100

    Feat: Adding a utility to splice a string so it can retrieve the file description

---
+++ src/utils/RetrieveNameFromFilePath.ts
@@ -0,0 +1,11 @@
+export function generateImageCaptionFromFilePath(filePath: string) {
+    const index = filePath.lastIndexOf('/');
+    let result = index !== -1 ? filePath.substring(index + 1) : filePath;
+    if (result.endsWith('.webp') || result.endsWith('jpeg')) {
+      result = result.slice(0, -5);
+    }
+    if (result.endsWith('.jpg')) {
+      result = result.slice(0, -4);
+    }
+    return result;
+}
